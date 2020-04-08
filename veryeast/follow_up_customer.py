9faede00e3375c77aeaf25f63e5215b3b7ca68cc
yuchengren <513082359@qq.com>
2020-04-08 19:46:58 +0800

    1.follow_up_customer，在点击下一页spinner消失后，再sleep，防止列表刷新不及时。

--- veryeast/follow_up_customer.py
+++ veryeast/follow_up_customer.py
@@ -128,6 +128,8 @@ while nextPageElement is None or nextPageElement.get_attribute("aria-disabled") 
         driver.execute_script("arguments[0].click();", nextPageElement)
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "ant-table-spin-holder")))
     WebDriverWait(driver, 10).until_not(EC.visibility_of_element_located((By.CLASS_NAME, "ant-table-spin-holder")))
+    if nextPageElement is not None:
+        time.sleep(1)
     listElement = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "ant-table-tbody")))
     WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "ant-table-row")))
     listRows = listElement.find_elements_by_class_name("ant-table-row")
