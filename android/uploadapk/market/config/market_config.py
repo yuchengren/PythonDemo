24d278fde869568f33950bd5bb287998b6ccd0b4
yuchengren <513082359@qq.com>
2020-04-29 10:16:27 +0800

    1.android目录脚本暂时删除，维护在内网。

--- android/uploadapk/market/config/market_config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-apk_zip_parent_path = os.environ['HOME'] + "/Downloads"
-apk_dir_name = "rebuildChannel"
-apk_zip_name = apk_dir_name + ".zip"
--- android/uploadapk/market/config/market_urls.py
+++
@@ -1,8 +0,0 @@
-
-
-xiaomi_market_apps_page = "https://dev.mi.com/myitems"
-oppo_market_apps_page = "https://open.oppomobile.com/home/management/app-admin#/resource/list"
-# vivo_market_apps_page = "https://dev.vivo.com.cn/appLists"
-vivo_market_apps_page = "https://id.vivo.com.cn/"
-tencent_market_apps_page = "http://op.open.qq.com/manage_centerv2/android"
-huawei_market_apps_page = "https://developer.huawei.com/consumer/cn/service/josp/agc/index.html#/myApp"
