b7b432088b84d038e55d8d295cfafee400ae0163
yuchengren <513082359@qq.com>
2020-06-12 18:11:21 +0800

    1.提交19。

--- veryeast/pipeline_follow_up_customer.groovy
+++ veryeast/pipeline_follow_up_customer.groovy
@@ -10,8 +10,8 @@ pipeline{
         string(name: 'follow_up_first_day_interval_today', defaultValue: '1', description: '将要跟进客户的日期距离今天的天数')
         string(name: 'query_follow_up_count_days', defaultValue: '5', description: '自动查询将要跟进日期开始往后的日期 跟进客户数量，限制查询的天数，再往后的没有查询到日期，默认当做客户数量为0')
         string(name: 'max_each_day_follow_up_count', defaultValue: '55', description: '每天可安排的客户最大数量')
-        booleanParam(name: 'max_captcha_recognise_times', defaultValue: '10', description: '验证码最多执行识别的次数')
-        string(name: 'is_tensorflow_recognise_captcha', defaultValue: True, description: '是否用tensorflow做验证码识别')
+        string(name: 'max_captcha_recognise_times', defaultValue: '10', description: '验证码最多执行识别的次数')
+        booleanParam(name: 'is_tensorflow_recognise_captcha', defaultValue: True, description: '是否用tensorflow做验证码识别')
     }
 
     stages{
