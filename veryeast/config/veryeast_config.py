321d3f8cf4766fc4d658d0e49ed502212be7e072
yuchegnren <513082359@qq.com>
2020-07-27 20:55:36 +0800

    1.测试59

--- veryeast/config/veryeast_config.py
+++ veryeast/config/veryeast_config.py
@@ -2,7 +2,8 @@
 import os
 
 # 截图保存路径
-SCREEN_IMG_DIR = os.environ['HOME'] + "/veryeast_img"
-SCREEN_IMG_ERROR_DIR = os.environ['HOME'] + "/veryeast_img_error"
+
+SCREEN_IMG_DIR = os.environ.get('HOME', 'root') + "/veryeast_img"
+SCREEN_IMG_ERROR_DIR = os.environ.get('HOME', 'root') + "/veryeast_img_error"
 HOST = "http://gateway.admin.dfwsgroup.com"
 BG_SYSTEM_URL = "http://admin.dfwsgroup.com/"
